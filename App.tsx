d5582b08deba04f6af9d4b48b8e8b1da48421e54
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-09 17:33:41 -0500

    add - states

--- App.tsx
+++ App.tsx
@@ -1,16 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Platform, SafeAreaView } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+	const [isWorking, setIsWorking] = useState(false);
+	const [time, setTime] = useState(25 * 60);
+	const [currtenTime, setCurrentTime] = useState('Pomo' || 'Short' || 'Long');
+
 	return (
 		<SafeAreaView style={styles.container}>
 			<View style={{ paddingTop: Platform.OS === 'android' ? 30 : 0 }}>
 				<Text style={styles.title}>Pomodoro</Text>
+				<Text style={styles.title}>{time}</Text>
 				<StatusBar style='dark' />
 			</View>
 		</SafeAreaView>
 	);
 }
+const colors = ['#F7DC6F', '#95fc51', '#EB984E'];
 
 const styles = StyleSheet.create({
 	container: {
