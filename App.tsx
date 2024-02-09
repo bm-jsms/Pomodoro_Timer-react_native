7988932b1f275a7e27aaaaf30fb403c2029cb877
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-09 18:07:38 -0500

    up - header

--- App.tsx
+++ App.tsx
@@ -6,13 +6,18 @@ import Headxer from './components/Headxer';
 export default function App() {
 	const [isWorking, setIsWorking] = useState(false);
 	const [time, setTime] = useState(25 * 60);
-	const [currtenTime, setCurrentTime] = useState('Pomo' || 'Short' || 'Long');
+	const [currentTime, setCurrentTime] = useState('Pomo' || 'Short' || 'Long');
 
 	return (
 		<SafeAreaView style={styles.container}>
 			<View style={{ paddingTop: Platform.OS === 'android' ? 30 : 0 }}>
 				<Text style={styles.title}>Pomodoro</Text>
-				<Headxer time={time} />
+				<Text style={styles.title}>{time}</Text>
+				<Headxer
+					setTime={setTime}
+					currentTime={currentTime}
+					setCurrentTime={setCurrentTime}
+				/>
 				<StatusBar style='dark' />
 			</View>
 		</SafeAreaView>
@@ -23,6 +28,7 @@ const colors = ['#F7DC6F', '#95fc51', '#EB984E'];
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		marginLeft: 8,
 	},
 	title: {
 		fontSize: 30,
--- components/Headxer.tsx
+++ components/Headxer.tsx
@@ -1,20 +1,38 @@
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 type HeadxerProps = {
-	time: number;
+	setTime: (time: number) => void;
+	currentTime: string;
+	setCurrentTime: (time: string) => void;
 };
 
 const options = ['Pomodoro', 'Short Break', 'Long Break'];
 
-const Headxer = ({ time }: HeadxerProps) => {
+const Headxer = ({ setTime, currentTime, setCurrentTime }: HeadxerProps) => {
+	const handlePress = (index: number) => {
+		const newTime = index === 0 ? 25 : index === 1 ? 5 : 15;
+
+		setCurrentTime(options[index]);
+		setTime(newTime * 60);
+	};
+
 	return (
 		<View style={{ flexDirection: 'row' }}>
 			{options.map((option, index) => (
-				<TouchableOpacity key={index} style={styles.itemStyle}>
+				<TouchableOpacity
+					key={index}
+					onPress={() => handlePress(index)}
+					style={[
+						styles.itemStyle,
+						currentTime !== option && {
+							opacity: 0.7,
+							borderColor: 'transparent',
+						},
+					]}
+				>
 					<Text>{option}</Text>
 				</TouchableOpacity>
 			))}
-			<Text>{time}</Text>
 		</View>
 	);
 };
@@ -25,6 +43,7 @@ const styles = StyleSheet.create({
 	itemStyle: {
 		borderWidth: 1,
 		borderColor: 'black',
+		borderRadius: 5,
 		padding: 5,
 		width: '33%',
 	},
